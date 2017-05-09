cde7fe87690a5b378311045c8054f015775e13e4
vijoz <913325132@qq.com>
2017-05-09 13:24:32 +0800

    修复v7冲突提交

--- ABanner/app/src/main/java/module/vijoz/com/abanner/MainActivity.java
+++ ABanner/app/src/main/java/module/vijoz/com/abanner/MainActivity.java
@@ -1,11 +1,11 @@
 package module.vijoz.com.abanner;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
 import static module.vijoz.com.abanner.R.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
