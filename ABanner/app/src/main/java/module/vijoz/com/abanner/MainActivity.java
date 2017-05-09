ad1786f5532a41a411efc80d10a6bf2ab0b620df
vijoz <913325132@qq.com>
2017-05-09 11:35:19 +0800

    banner的moudle第一次提交

---
+++ ABanner/app/src/main/java/module/vijoz/com/abanner/MainActivity.java
@@ -0,0 +1,15 @@
+package module.vijoz.com.abanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static module.vijoz.com.abanner.R.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+    }
+}
